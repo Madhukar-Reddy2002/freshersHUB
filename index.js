8e906ab502ca3cb9ae5f90a2b11ba4fd2c9fb346
Madhukar <madhushashi2727@gmail.com>
2023-11-25 21:40:45 +0530

    cookie

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var createError = require('http-errors');
 var express = require('express');
 var expressSession = require('express-session');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); // Import cookie-parser
 var logger = require('morgan');
 var passport = require('passport');
 const ejs = require('ejs');
@@ -18,11 +18,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Use cookie-parser for local session storage
+app.use(cookieParser());
 app.use(expressSession({
   secret: "secret",
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser());
@@ -30,7 +33,6 @@ passport.deserializeUser(usersRouter.deserializeUser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
